03b592819eefe6833af8509e49d16a13e5fdc41b
Viniales12 <viniales12@gmail.com>
2023-07-30 19:38:55 +0200

    add new status type

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, status
+from fastapi import FastAPI, Depends, status, Response
 import models, schemas
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -32,7 +32,10 @@ def all_players(db: Session = Depends(get_db)):
     return players
 
 
-@app.get('/player/{id}')
-def player(id, db: Session = Depends(get_db)):
+@app.get('/player/{id}', status_code=status.HTTP_200_OK)
+def player(id, response: Response,db: Session = Depends(get_db)):
     player = db.query(models.Player).filter(models.Player.id == id).first()
+    if not player:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {'detail':f'Player with the id {id} is not available'}
     return player
