23f0a703e850b97c29648786ae5a1953c4e4ba59
Viniales12 <viniales12@gmail.com>
2023-08-06 21:17:13 +0200

    add authentication

--- routers/authentication.py
+++ routers/authentication.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
 import schemas, database, models
 from tokens import token
 from sqlalchemy.orm import Session
@@ -12,7 +13,7 @@ get_db = database.get_db
 
 
 @router.post('/login')
-def login(request: schemas.Login, db: Session = Depends(get_db)):
+def login(request: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.email == request.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Invalid user')
