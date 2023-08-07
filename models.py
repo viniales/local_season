e2bef3851a0c46906960034c8a3c072145e94d68
Viniales12 <viniales12@gmail.com>
2023-08-07 18:29:56 +0200

    add points models

--- models.py
+++ models.py
@@ -26,3 +26,16 @@ class User(Base):
     password = Column(String)
 
     players = relationship("Player", back_populates="creator")
+
+    points = Column(Integer, ForeignKey("user_points.points"))
+    creator = relationship("UsersPoints", back_populates="user_points")
+
+
+class UsersPoints(Base):
+    __tablename__ = 'user_points'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    points = Column(Integer)
+    #
+    user_points = relationship("User", back_populates="creator")
