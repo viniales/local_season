941438db79ec2790f26f391f97f5e309d74f5bfb
Viniales12 <viniales12@gmail.com>
2023-08-14 17:23:41 +0200

    delete player schemas

--- schemas.py
+++ schemas.py
@@ -7,42 +7,18 @@ class Points(BaseModel):
     points: int
 
 
-class Player(BaseModel):
-    name: str
-    surname: str
-    age: int
-    team: str
-    nationality: str
-
-
 class User(BaseModel):
     name: str
     email: str
     password: str
 
 
-class ShowUserInPlayers(BaseModel):
-    name: str
-    email: str
-
-
 class ShowUser(BaseModel):
     name: str
     email: str
-    players: List[Player] = []
     points: List[Points] = []
 
     # "players" must be the same variable as in Models.py file
-
-
-class ShowPlayer(BaseModel):
-    name: str
-    surname: str
-    age: int
-    team: str
-    nationality: str
-    creator: ShowUserInPlayers
-    # "creator" must be the same variable as in Models.py file
 
 
 class Login(BaseModel):
