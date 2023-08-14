a13c3f1745802100bcbb5484a3bb2d6bb7bd2b4f
Viniales12 <viniales12@gmail.com>
2023-08-14 17:24:14 +0200

    delete player models

--- models.py
+++ models.py
@@ -3,21 +3,6 @@ from database import Base
 from sqlalchemy.orm import relationship
 
 
-#
-# class Player(Base):
-#     __tablename__ = 'players_table'
-#
-#     id = Column(Integer, primary_key=True, index=True)
-#     name = Column(String)
-#     surname = Column(String)
-#     age = Column(Integer)
-#     team = Column(String)
-#     nationality = Column(String)
-#     user_id = Column(Integer, ForeignKey("users.id"))
-#
-#     creator = relationship("User", back_populates="players")
-
-
 class User(Base):
     __tablename__ = 'users'
 
@@ -27,11 +12,7 @@ class User(Base):
     password = Column(String)
     points = Column(Integer, default=0)
 
-    # players = relationship("Player", back_populates="creator")
-
 
 class Matches(Base):
     __tablename__ = 'matches'
     id = Column(Integer, primary_key=True, index=True)
-
-
