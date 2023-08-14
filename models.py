e47a8856154464b742c8f0d7b2dd88e750177399
Viniales12 <viniales12@gmail.com>
2023-08-14 17:45:12 +0200

    new models

--- models.py
+++ models.py
@@ -12,7 +12,27 @@ class User(Base):
     password = Column(String)
     points = Column(Integer, default=0)
 
+    match_betting = relationship("MatchBetting", back_populates="user")
 
-class Matches(Base):
+
+class Match(Base):
     __tablename__ = 'matches'
     id = Column(Integer, primary_key=True, index=True)
+    team1 = Column(String)
+    team2 = Column(String)
+    result_team1 = Column(Integer)
+    result_team2 = Column(Integer)
+
+    match_betting = relationship("MatchBetting", back_populates="match")
+
+
+class MatchBetting(Base):
+    __tablename__ = 'match_batting'
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    match_id = Column(Integer, ForeignKey("matches.id"))
+    prediction_score_team1 = Column(Integer)
+    prediction_score_team2 = Column(Integer)
+
+    user = relationship("User", back_populates='match_betting')
+    match = relationship("Match", back_populates='match_betting')
