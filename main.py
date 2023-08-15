fe912d3c388a10929f4a16937e0a031cfd833393
Viniales12 <viniales12@gmail.com>
2023-08-15 17:15:47 +0200

    code refresh

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 import models
 from database import engine
-from routers import user, authentication, match_betting
+from routers import user, authentication, match_betting, matches
 
 app = FastAPI()
 
@@ -10,3 +10,4 @@ models.Base.metadata.create_all(engine)
 app.include_router(authentication.router)
 app.include_router(user.router)
 app.include_router(match_betting.router)
+app.include_router(matches.router)
