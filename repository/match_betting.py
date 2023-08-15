3ba35bd19b88295bf0be9f9908e7b71744e62199
Viniales12 <viniales12@gmail.com>
2023-08-15 16:56:31 +0200

    add betting repository

---
+++ repository/match_betting.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import Session
+import models, schemas
+from fastapi import status, HTTPException
+from hashing import Hash
+
+
+# def check_exist(request: schemas.MatchBetting, db: Session):
+#     return db.query(models.User).filter(models.User.name == request.name).first()
+def create(request: schemas.MatchBetting, db: Session):
+    # if check_exist(request, db):
+    #     raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail='The User already exist')
+
+    new_match = models.MatchBetting(prediction_score_team1=request.team1, prediction_score_team2=request.team2)
+    db.add(new_match)
+    db.commit()
+    db.refresh(new_match)
+
+    return new_match
