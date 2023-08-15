4a98b239671b7d2d1af12ae97d9aadcd26be5a0b
Viniales12 <viniales12@gmail.com>
2023-08-15 17:16:06 +0200

    add matches schemas

--- schemas.py
+++ schemas.py
@@ -33,3 +33,16 @@ class Token(BaseModel):
 
 class TokenData(BaseModel):
     username: str | None = None
+
+
+class MatchBetting(BaseModel):
+    team1: int
+    team2: int
+
+
+class Match(BaseModel):
+    team1: str
+    team2: str
+    result_team1: int
+    result_team2: int
+    date: str
