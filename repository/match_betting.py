e5cbb9107a9a8722d73914513959527db085cdf3
Viniales12 <viniales12@gmail.com>
2023-08-16 23:33:00 +0200

    add matches repository

--- repository/match_betting.py
+++ repository/match_betting.py
@@ -6,11 +6,10 @@ from hashing import Hash
 
 # def check_exist(request: schemas.MatchBetting, db: Session):
 #     return db.query(models.User).filter(models.User.name == request.name).first()
-def create(request: schemas.MatchBetting, db: Session):
+def create(request: schemas.MatchBetting, db: Session, user):
     # if check_exist(request, db):
     #     raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail='The User already exist')
-
-    new_match = models.MatchBetting(prediction_score_team1=request.team1, prediction_score_team2=request.team2)
+    new_match = models.MatchBetting(prediction_score_team1=request.team1, prediction_score_team2=request.team2, match_id=request.match_id, user_id=user)
     db.add(new_match)
     db.commit()
     db.refresh(new_match)
