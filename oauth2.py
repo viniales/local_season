c7604b519390eaf28d014db1f3d0276d084d7cd3
Viniales12 <viniales12@gmail.com>
2023-08-29 13:11:46 +0200

    add new functionality

--- oauth2.py
+++ oauth2.py
@@ -1,11 +1,3 @@
-from fastapi import Depends, HTTPException, status
-from tokens import token
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-
-from datetime import datetime, timedelta
-from jose import JWTError, jwt
-import schemas
-
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
 import schemas, database, models
@@ -13,16 +5,33 @@ from fastapi import Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')  # the path api for login
+
+# SECRET_KEY
+# Algorithm
+# Expiration time of the token, aka how long the user can be logged in
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
 def create_access_token(data: dict):
-    to_encode = data.copy()
+    """
+    Creates a JWT (JSON Web Token) that includes the provided user data. The token has a
+    preset expiration time (defined by ACCESS_TOKEN_EXPIRE_MINUTES). This function is typically
+    used during the authentication process, where it creates a token for a successfully
+    authenticated user, which the user can then use for authenticating subsequent requests.
 
+    Args:
+        data (dict): A dictionary that must include the user details. The 'user_id' is an example
+        of a detail that can be included. This data will be embedded in the JWT payload.
+
+    Returns:
+        str: A JWT as a string. This JWT includes the user details and an expiration time.
+        This token can be returned to the client during the login process or token refresh process.
+    """
+    to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
 
@@ -31,38 +40,60 @@ def create_access_token(data: dict):
     return encoded_jwt
 
 
-def verify_access_token(token: str, credentials_exception):
-    try:
+def verify_access_token(token: str, credentials_exception: HTTPException):
+    """
+    Verifies and decodes a JWT token. If verification is successful, a TokenData
+    object is returned. If token verification fails, a credentials_exception is
+    raised.
 
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        email: str = payload.get("sub")
-        if email is None:
+    Args:
+        token (str): The JWT token to verify and decode.
+        credentials_exception: The exception to be raised if token verification fails.
+
+    Returns:
+        TokenData: A Pydantic model containing the decoded information from the JWT.
+    """
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])  # decodes the token from user
+        id: str = payload.get("user_id")
+        # if no 'id' in token, then throw an error
+        if id is None:
             raise credentials_exception
-        token_data = schemas.TokenData(email=email)
+        token_data = schemas.TokenData(id=id)  # validates the schema
     except JWTError:
         raise credentials_exception
-    print(token_data)
-    return token_data
+
+    return token_data  # in this case just the 'id' but extra fields may be added to the payload
 
 
+# initializes credentials_exception and calls the verify token method with the users token
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)):
-    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                                          detail=f"Could not validate credentials",
-                                          headers={"WWW-Authenticate": "Bearer"})
+    """
+    Authenticates a user using a provided JWT token and retrieves the user's details
+    from a database. If token verification fails, a HTTP 401 Unauthorized error is
+    returned.
+
+    Args:
+        token (str): The JWT token to verify. This is provided using FastAPI's
+        dependency injection system, and would typically come from the 'Authorization'
+        header in the HTTP request.
+
+        db (Session): The database session to use for querying the database. This is
+        provided using FastAPI's dependency injection system.
+
+    Returns:
+        models.User: The authenticated user's details retrieved from the database.
+
+    Raises:
+        HTTPException: If token verification fails, an HTTP 401 Unauthorized
+        error is returned.
+    """
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail=f"Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"})
 
     token = verify_access_token(token, credentials_exception)
-    print(token)
+    user = db.query(models.User).filter(models.User.id == token.id).first()
 
-
-    user = db.query(models.User).filter(models.User.email == token.username).first()
-    print(user)
     return user
-
-# def get_current_user(data: str = Depends(oauth2_scheme)):
-#     credentials_exception = HTTPException(
-#         status_code=status.HTTP_401_UNAUTHORIZED,
-#         detail="Could not validate credentials",
-#         headers={"WWW-Authenticate": "Bearer"},
-#     )
-#
-#     return token.verify_token(data, credentials_exception)
