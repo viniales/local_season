f85132b1d77ccfdb85e165b4523adfaede26c060
Viniales12 <viniales12@gmail.com>
2023-08-29 13:10:38 +0200

    new hashing

---
+++ utils.py
@@ -0,0 +1,13 @@
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+# hashes plain password
+def hash(password: str):
+    return pwd_context.hash(password)
+
+
+# verifies the user's input and the hashed password by hashing the user input and comparing
+def verify(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
