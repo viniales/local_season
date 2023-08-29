9f7bb4b5325b095ddf86a99a199d852ec8302844
Viniales12 <viniales12@gmail.com>
2023-08-29 13:12:04 +0200

    code refresh

--- routers/user.py
+++ routers/user.py
@@ -1,21 +1,35 @@
-from fastapi import APIRouter, Depends
+from fastapi import status, HTTPException, Depends, APIRouter
+import models, schemas, utils
 from sqlalchemy.orm import Session
-import database, schemas
-from repository import user as use
+from database import get_db
 
+# prefix = adds the string to the beginning of each path op
+# tags = is for the docs website for this api, it groups these path ops under 'Users' in the docs
 router = APIRouter(
-    prefix='/user',
-    tags=['User']
+    prefix="/users",
+    tags=['Users']
 )
 
-get_db = database.get_db
+
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    # hash the password - user.password
+    hashed_password = utils.hash(user.password)
+    user.password = hashed_password
+
+    new_user = models.User(**user.dict())
+    db.add(new_user)  # add to database
+    db.commit()  # then commit it
+    db.refresh(new_user)
+
+    return new_user
 
 
-@router.post('/', response_model=schemas.ShowUser)
-def create_user(request: schemas.User, db: Session = Depends(get_db)):
-    return use.create(request, db)
+@router.get("/{id}", response_model=schemas.UserOut)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"User with id: {id}")
 
-
-@router.get('/{id}', response_model=schemas.ShowUser)
-def show_user(id: int, db: Session = Depends(get_db)):
-    return use.get_one(id, db)
+    return user
