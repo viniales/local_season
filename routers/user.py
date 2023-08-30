398c9d940b26c89cb52a05bee41770d87b666405
Viniales12 <viniales12@gmail.com>
2023-08-30 18:22:52 +0200

    add user score

--- routers/user.py
+++ routers/user.py
@@ -16,8 +16,8 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     # hash the password - user.password
     hashed_password = utils.hash(user.password)
     user.password = hashed_password
-
     new_user = models.User(**user.dict())
+    new_user.score = 0
     db.add(new_user)  # add to database
     db.commit()  # then commit it
     db.refresh(new_user)
