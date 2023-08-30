03772eaceebab78abe4b7ce0447494e147772e62
Viniales12 <viniales12@gmail.com>
2023-08-30 18:23:00 +0200

    add user score

--- repository/user.py
+++ repository/user.py
@@ -1,7 +1,6 @@
 from sqlalchemy.orm import Session
 import models, schemas
 from fastapi import status, HTTPException
-from hashing import Hash
 
 
 def check_exist(request: schemas.User, db: Session):
@@ -12,7 +11,7 @@ def create(request: schemas.User, db: Session):
     if check_exist(request, db):
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail='The User already exist')
 
-    new_user = models.User(name=request.name, email=request.email, password=Hash.bcrypt(request.password), points=0)
+    new_user = models.User(name=request.name, email=request.email, password=Hash.bcrypt(request.password))
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
