929b323e8f1cde4de2ca4976d45ce2408d84e315
Viniales12 <viniales12@gmail.com>
2023-08-31 10:36:04 +0200

    add calculate score

--- repository/calculate_score.py
+++ repository/calculate_score.py
@@ -13,4 +13,13 @@ def calculate_score(db: Session):
 
         match = db.query(models.Match).filter(models.Match.id == match_id)
         if match:
-            pass
+            result_team1 = match.result_team1
+            result_team2 = match.result_team2
+
+            if result_team1 is not None and result_team2 is not None:
+                correct_prediction = (user_prediction_team1 == result_team1 and user_prediction_team2 == result_team2)
+
+                if correct_prediction:
+                    user = db.query(models.User).filter(models.User.id == user_id)
+                    if user:
+                        user.score += 1
