7d5781d2604cd68e5762172a1b6838b2e071c38f
Viniales12 <viniales12@gmail.com>
2023-08-31 11:43:48 +0200

    add new functionality(preventing the addition of additional points for the same bet)

--- routers/calculate_score.py
+++ routers/calculate_score.py
@@ -3,7 +3,7 @@ from database import get_db
 from sqlalchemy.orm import Session
 from repository import calculate_score
 
-router = APIRouter(prefix='calculate_score', tags=['Calculate score'])
+router = APIRouter(prefix='/calculate_score', tags=['Calculate score'])
 
 
 @router.get('/', status_code=status.HTTP_200_OK)
