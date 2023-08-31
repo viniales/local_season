9158e3f9766243b5535da4edf1366089917a411f
Viniales12 <viniales12@gmail.com>
2023-08-31 11:43:41 +0200

    add new functionality(preventing the addition of additional points for the same bet)

--- repository/calculate_score.py
+++ repository/calculate_score.py
@@ -10,18 +10,22 @@ def calculate_score(db: Session):
         user_id = match_betting.owner_id
         user_prediction_team1 = match_betting.prediction_score_team1
         user_prediction_team2 = match_betting.prediction_score_team2
+        points_awarded = match_betting.points_awarded
 
-        match = db.query(models.Match).filter(models.Match.id == match_id)
-        if match:
-            result_team1 = match.result_team1
-            result_team2 = match.result_team2
+        if not points_awarded:
+            match = db.query(models.Match).filter(models.Match.id == match_id).first()
+            if match:
+                result_team1 = match.result_team1
+                result_team2 = match.result_team2
 
-            if result_team1 is not None and result_team2 is not None:
-                correct_prediction = (user_prediction_team1 == result_team1 and user_prediction_team2 == result_team2)
+                if result_team1 is not None and result_team2 is not None:
+                    correct_prediction = (
+                                user_prediction_team1 == result_team1 and user_prediction_team2 == result_team2)
 
-                if correct_prediction:
-                    user = db.query(models.User).filter(models.User.id == user_id)
-                    if user:
-                        user.score += 1
+                    if correct_prediction:
+                        user = db.query(models.User).filter(models.User.id == user_id).first()
+                        if user:
+                            user.score += 1
+                            match_betting.points_awarded = True
 
     db.commit()
