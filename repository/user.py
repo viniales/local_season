38b61bbb3a0a28c123c2d7f3c101e063ff92826a
Viniales12 <viniales12@gmail.com>
2023-09-04 23:58:25 +0200

    code refresh

--- repository/user.py
+++ repository/user.py
@@ -7,16 +7,20 @@ def check_exist(request: schemas.User, db: Session):
     return db.query(models.User).filter(models.User.name == request.name).first()
 
 
-def create(request: schemas.User, db: Session):
-    if check_exist(request, db):
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail='The User already exist')
-
-    new_user = models.User(name=request.name, email=request.email, password=Hash.bcrypt(request.password))
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-
-    return new_user
-
-
-
+# def create(request: schemas.User, db: Session):
+#     if check_exist(request, db):
+#         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail='The User already exist')
+#
+#     new_user = models.User(name=request.name, email=request.email, password=Hash.bcrypt(request.password))
+#     db.add(new_user)
+#     db.commit()
+#     db.refresh(new_user)
+#
+#     return new_user
+#
+#
+# def get_one(id, db: Session):
+#     user = db.query(models.User).filter(models.User.id == id).first()
+#     if not user:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'User with the id {id} is not available')
+#     return user
