bbf20607a0d40a6505cdcf9cc26ec5fa6ad0ff73
Viniales12 <viniales12@gmail.com>
2023-09-12 10:51:04 +0200

    code refresh

--- routers/user.py
+++ routers/user.py
@@ -17,9 +17,4 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
 @router.get("/{id}", response_model=schemas.UserOut)
 def get_user(id: int, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.id == id).first()
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"User with id: {id}")
-
-    return user
+    return user_repository.get_user(id, db)
