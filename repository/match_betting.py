918e2b0dc81f503e55e58cfbdb8960fa5dd93cf5
Viniales12 <viniales12@gmail.com>
2023-10-26 09:38:23 +0200

    code refactor

--- repository/match_betting.py
+++ repository/match_betting.py
@@ -3,15 +3,19 @@ import models, schemas
 from fastapi import status, HTTPException
 
 
-# def check_exist(request: schemas.MatchBetting, db: Session):
-#     return db.query(models.User).filter(models.User.name == request.name).first()
-def create(request: schemas.MatchBetting, db: Session, user):
-    # if check_exist(request, db):
-    #     raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail='The User already exist')
-    new_match = models.MatchBetting(prediction_score_team1=request.team1, prediction_score_team2=request.team2,
-                                    match_id=request.match_id, user_id=user)
-    db.add(new_match)
+def create(post: schemas.MatchBettingCreate, db: Session, current_user):
+    new_post = models.MatchBetting(owner_id=current_user.id, **post.dict())
+    db.add(new_post)
     db.commit()
-    db.refresh(new_match)
+    db.refresh(new_post)
+    return new_post
 
-    return new_match
+#
+# def create(request: schemas.MatchBetting, db: Session, user):
+#     new_match = models.MatchBetting(prediction_score_team1=request.team1, prediction_score_team2=request.team2,
+#                                     match_id=request.match_id, user_id=user)
+#     db.add(new_match)
+#     db.commit()
+#     db.refresh(new_match)
+#
+#     return new_match
